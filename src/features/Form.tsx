b5539d872d4e0fe0254d0fd26d05832db00fa521
Nivesh <46673471+N1ve5h@users.noreply.github.com>
2024-12-06 13:31:20 +0000

    Form CheckBox

--- src/features/Form.tsx
+++ src/features/Form.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
 import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage, Form as FormElement } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -10,6 +11,7 @@ const formSchema = z.object({
     message: "To do must be at least 3 characters.",
   })
   .max(20, {message: "To do must be at no more than 20 characters."}),
+  isDeadline: z.boolean()
 })
  
 export default function Form({add}) {
@@ -27,12 +29,12 @@ export default function Form({add}) {
 
   return (
     <FormElement {...formData}>
-      <form onSubmit={formData.handleSubmit(onSubmit)} className='flex flex-row justify-around items-center'>
+      <form onSubmit={formData.handleSubmit(onSubmit)} className='w-11/12 flex flex-col justify-around items-start gap-4'>
         <FormField
           control={formData.control}
           name="Todo"
           render={({ field }) => (
-            <FormItem className="w-2/3">
+            <FormItem className="w-full">
               <FormLabel>To do</FormLabel>
               <FormControl>
                 <Input {...field} />
@@ -41,6 +43,25 @@ export default function Form({add}) {
                 Enter your task
               </FormDescription>
               <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={formData.control}
+          name="isDeadline"
+          render={({ field }) => (
+            <FormItem className="w-1/3 flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+              <FormControl>
+                <Checkbox
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <div className="leading-none">
+                <FormLabel>
+                  Set a deadline
+                </FormLabel>
+              </div>
             </FormItem>
           )}
         />
